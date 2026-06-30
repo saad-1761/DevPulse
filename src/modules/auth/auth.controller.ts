f7f3e09a72013656f8a1e9cde3988f17c553d7cb
MD Sadbin Hasan <sadbin1761@gmail.com>
2026-06-30 16:17:03 +0600

    login api created

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import { authService } from "./auth.service";
+import sendResponse from "../../utility/sendResponse";
+
+const loginUser = async (req: Request, res: Response) => {
+  try {
+    const result = await authService.loginUserIntoDB(req.body);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Login successfully!",
+      data: result,
+    });
+
+  } catch (error: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+export const authController = {
+  loginUser,
+};
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import bcrypt from "bcryptjs";
+import { pool } from "./../../db/index";
+
+import jwt from "jsonwebtoken";
+import config from "../../config";
+
+const loginUserIntoDB = async (payload: {
+  email: string;
+  password: string;
+}) => {
+  const { email, password } = payload;
+  const userData = await pool.query(
+    `
+    SELECT * FROM users WHERE email=$1
+    `,
+    [email],
+  );
+  if (userData.rows.length === 0) {
+    throw new Error("Invalid Credentials!");
+  }
+
+
+  const user = userData.rows[0];
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) {
+    throw new Error("Invalid Credentials!");
+  }
+
+  const jwtpayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  };
+
+  const accessToken = jwt.sign(jwtpayload, config.secret as string, {
+    expiresIn: "1d",
+  });
+
+  delete user.password;
+  return { 
+    token:accessToken, user
+   };
+};
+
+export const authService = {
+  loginUserIntoDB,
+};
